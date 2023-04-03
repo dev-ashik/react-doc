86b1f08c07430299ed59e79e7c71697d581f8565
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-04-03 10:36:02 +0800

    component, props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+let guest = 0;
+
+function Cup() {
+  guest = guest + 1;
+
+  return <h2>Tea cup for guest :{guest}</h2>
+}
+
 function App() {
   return (
     <div className="App">
-      This is a COMPONENT
+      <Cup/>
+      <Cup/>
+      <Cup/>
     </div>
   );
 }
